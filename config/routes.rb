95a31b567b9063f1dd15a58357c4d2318d4e569b
Mphatso Lichapa <lichapau@gmail.com>
2022-05-24 12:04:59 +0200

    Installed devies and generated devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'food/index'
   get 'pages/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :foods
+  end
   root 'pages#hello'
+
+
 end

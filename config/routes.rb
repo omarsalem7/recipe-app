eacd9e2bb419e4dfd15c1c503b2da54a7cd87ef8
omarsalem7 <omarsalem0721@gmail.com>
2022-05-24 14:19:41 +0200

    Add crud for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/hello'
+  resources :recipes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'pages#hello'
+  root 'recipes#index'
 end

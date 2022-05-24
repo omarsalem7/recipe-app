f807c81cde8e1d0a82ecd0199ce68472884b6793
Mphatso Lichapa <lichapau@gmail.com>
2022-05-24 11:31:07 +0200

    created model for food

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/index'
   get 'food/index'
   get 'pages/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

6751294a9c8e7ac7566e903ad1e001955d6710d9
omarsalem7 <omarsalem0721@gmail.com>
2022-05-24 17:04:05 +0200

    Improve UI for recipts

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,19 +20,18 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
       else
-        format.html { render :new, alert: 'An error has occurred while creating the recipt, try again'}
+        format.html { render :new, alert: 'An error has occurred while creating the recipt, try again' }
       end
     end
   end
-
 
   def update
     @recipe = Recipe.find(params[:id])
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
       else
         format.html { render :edit, alert: 'An error has occurred while updating the recipt' }
       end
@@ -43,13 +42,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     respond_to do |format|
-      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
     end
   end
 
   private
 
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end

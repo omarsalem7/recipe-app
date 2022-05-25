c2b01149a4916a51866f02f6c7c501b60928c7cb
omarsalem7 <omarsalem0721@gmail.com>
2022-05-25 18:00:39 +0200

    Add user when add recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,7 +18,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-
+    @recipe.user_id = current_user.id
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }

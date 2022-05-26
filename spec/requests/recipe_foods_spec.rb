d79bb977a5f4bb847d02a42375ea66480fd0c6bd
omarsalem7 <omarsalem0721@gmail.com>
2022-05-26 13:26:57 +0200

    fix linetrs

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "RecipeFoods", type: :request do
-  describe "GET /destory" do
-    it "returns http success" do
-      get "/recipe_foods/destory"
+RSpec.describe 'RecipeFoods', type: :request do
+  describe 'GET /destory' do
+    it 'returns http success' do
+      get '/recipe_foods/destory'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/recipe_foods/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/recipe_foods/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end

0604c5e40db1f7c815bc21e7103731599dd77626
Mphatso Lichapa <lichapau@gmail.com>
2022-05-26 10:53:54 +0200

    fixed migration issues then run bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'shopping_lists/index'
   devise_for :users
   resources :recipes
-  get '/public_recipes', to: 'recipes#public'
+ 
   resources :users do
     resources :foods
   end
+
+  resources :public_recipes, only: %i[index]
+  resources :shopping_lists, only: %i[index]
+  resources :recipe_foods, only: %i[new create destroy]
+  
 
   root 'recipes#public'
 end

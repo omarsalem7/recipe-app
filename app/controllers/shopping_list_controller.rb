2229432a524eb34fc9a3c369e84633f95097051c
Mphatso Lichapa <lichapau@gmail.com>
2022-05-26 07:46:45 +0200

    Added shopping list

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,18 @@
+class ShoppingListController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    @ingredients = RecipeFood.where(recipe_id: @recipes.map(&:id))
+    @foods = Food.where(id: @ingredients.map(&:food_id))
+  end
+
+  def total_price
+    price = 0
+    @ingredients.each do |recipe_food|
+      price += Food.find(recipe_food.food_id).price * recipe_food.quantity
+    end
+    price
+  end
+
+  helper_method :total_price
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'shopping_list/index'
   devise_for :users
   resources :recipes
   get '/public_recipes', to: 'recipes#public'

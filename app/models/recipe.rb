bbd09224b6e26ddd97f271c62c8247506b144691
omarsalem7 <omarsalem0721@gmail.com>
2022-05-26 14:22:05 +0200

    Add destory and edit btn and fix destory relation in model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true

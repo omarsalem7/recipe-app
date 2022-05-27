0d0498e64c8685c734674512cf44798c6c53e8ad
Mphatso Lichapa <lichapau@gmail.com>
2022-05-27 11:00:47 +0200

    Added database cleaner gem

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rails-controller-testing'
   gem 'rspec-rails', '~> 6.0.0.rc1'
+  gem "database_cleaner"
 end
 
 group :development do
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @user = User.create!(name: 'Mphatso', email: 'mphatso@mail.com', password: 'password')
+    food = Food.create!(name: 'Casava', measurement_unit: 'unit', price: 2, user_id: @user.id)
+  end
+
+  before(:each) { subject.save }
+
+  it 'Name is not nil' do    
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end

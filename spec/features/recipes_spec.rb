be18280e0ec3a985c6a5247848643ab6d639bf5c
omarsalem7 <omarsalem0721@gmail.com>
2022-05-27 11:52:56 +0200

    test revipes

---
+++ spec/features/recipes_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Post index page', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'Omar', email: 'omara@gmail.com', password: '123455')
+    @recipe = Recipe.create(name: 'hello', preparation_time: '4:00', cooking_time: '2:00', description: 'chocolat', public: true, user: @user)
+    visit new_user_session_path
+    fill_in 'Email', with: 'omara@gmail.com'
+    fill_in 'Password', with: '123455'
+    click_button 'Log in'
+    visit recipes_path 
+  end
+  describe 'tests for view recipes#index' do
+    it 'name of user.' do
+      expect(page).to have_content 'Omar'
+    end
+
+    it 'see a header of table (name)' do
+      expect(page).to have_content 'Name'
+    end
+
+    it 'see the recipes has Preparation time' do
+      expect(page).to have_content 'Preparation time'
+    end
+
+    it 'When I click a user post, it redirects me to that post s show page.' do
+      visit recipe_path(@recipe.id)
+      expect(page).to have_content 'Description'
+      expect(page).to have_content 'recipe'
+      expect(page).to have_content 'Destroy'
+    end
+  end
+end

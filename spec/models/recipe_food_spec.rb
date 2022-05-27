5aeeac78d00babad47bad4b937716cc6ea51c1a6
omarsalem7 <omarsalem0721@gmail.com>
2022-05-27 11:40:33 +0200

    Add specs for login page

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Omar', email: 'omara@gmail.com', password: '123455')
+    expect(@user).to be_valid
+    @recipe = Recipe.create(name: 'hello', preparation_time: '4:00', cooking_time: '2:00', description: 'chocolat', public: true, user: @user)
+    @food = Food.create(name: 'Mango', measurement_unit: 'grams', price: 3, user: @user)
+    @recipe_food = RecipeFood.create(quantity: 2, recipe: @recipe, food: @food)
+  end
+
+  it 'spec create a recipefood' do
+    expect(@recipe_food).to be_valid
+    expect(@recipe_food.quantity).to eq 2
+    expect(@recipe_food.recipe).to eq @recipe
+    expect(@recipe_food.food).to eq @food
+  end
 end
